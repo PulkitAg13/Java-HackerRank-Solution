31ccb5d3c7a67b522c95f803aa952a7012bcfbed
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-24 21:40:51 +0530

    Update Java_Loops_II.java

--- Java_Loops_II.java
+++ Java_Loops_II.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Java_Loops_II {
-    
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int[] arr = new int[n];
+            int term = a;
+            for (int j = 0; j < n; j++) {
+                term = term + (int)Math.pow(2, j) * b;
+                arr[j] = term;  
+            }
+        
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            
+            System.out.println();
+        }
+        
+        in.close();
+    }
 }
