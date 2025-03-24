9a57c74ef54fa0c003e38c0a0ded1d8ea577e92a
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-24 21:49:23 +0530

    Update Java_End_Of_File.java

--- Java_End_Of_File.java
+++ Java_End_Of_File.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Java_End_Of_File {
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int lineNumber = 1;
+        
+        while (scanner.hasNextLine()) {
+            System.out.println(lineNumber + " " + scanner.nextLine());
+            lineNumber++;
+        }
+        
+        scanner.close();
+    }
 }
