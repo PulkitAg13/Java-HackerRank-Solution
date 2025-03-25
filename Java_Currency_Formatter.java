4e3fdc9d16334bfd85bf6b55e6b8869e4a9f58f1
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-25 22:38:28 +0530

    Update Java_Currency_Formatter.java

--- Java_Currency_Formatter.java
+++ Java_Currency_Formatter.java
@@ -1,3 +1,32 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Java_Currency_Formatter {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        Locale usLocale = Locale.US;
+        Locale indiaLocale = new Locale("en", "IN");
+        Locale chinaLocale = Locale.CHINA;
+        Locale franceLocale = Locale.FRANCE;
+
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(usLocale);
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(chinaLocale);
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(franceLocale);
+        
+        String us = usFormat.format(payment);
+        String india = indiaFormat.format(payment);
+        String china = chinaFormat.format(payment);
+        String france = franceFormat.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
 }
