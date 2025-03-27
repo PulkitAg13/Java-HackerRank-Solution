34d17b42419ef9abb57934d0794fb8e6ab51b67e
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-27 22:24:12 +0530

    Update Java_Substring_Comparisons.java

--- Java_Substring_Comparisons.java
+++ Java_Substring_Comparisons.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Java_Substring_Comparisons {
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = s.substring(0, k);
@@ -20,5 +22,13 @@ public class Java_Substring_Comparisons {
         }
         
         return smallest + "\n" + largest;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
     }
 }
