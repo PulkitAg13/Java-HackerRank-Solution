f01393cb6c8de55e3d195835729cb47f6613945e
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-27 22:18:53 +0530

    Update Java_Strings_Introduction.java

--- Java_Strings_Introduction.java
+++ Java_Strings_Introduction.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Java_Strings_Introduction {
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
