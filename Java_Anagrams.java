fdac897030d353ae36a2348a3dbf0108c1fa89f6
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-28 22:12:18 +0530

    Update Java_Anagrams.java
    finalize the code

--- Java_Anagrams.java
+++ Java_Anagrams.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Java_Anagrams {
     static boolean isAnagram(String a, String b) {
         // Complete the function
@@ -35,4 +37,15 @@ public class Java_Anagrams {
             }
         }
     }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
 }
+
