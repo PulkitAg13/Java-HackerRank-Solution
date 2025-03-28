900b6f64f2dfb1e24ca6c1d71b5ee54908b246e2
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-28 22:08:50 +0530

    Update Java_String_Reverse.java

--- Java_String_Reverse.java
+++ Java_String_Reverse.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Java_String_Reverse {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String reversed = new StringBuilder(A).reverse().toString();
+        if(reversed.equals(A)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+        sc.close();
+    }
 }
