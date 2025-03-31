0ec63f6edd83c6de067226f6ee0063a5b9c5bf44
PulkitAg13 <pulkit.ag13@gmail.com>
2025-03-31 23:39:23 +0530

    Update Java_String_Tokens.java

--- Java_String_Tokens.java
+++ Java_String_Tokens.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Java_String_Tokens {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        
+        s = s.trim();
+
+        
+        if (s.isEmpty()) {  
+            System.out.println(0);
+        }
+        else {
+            String[] tokens = s.split("[^A-Za-z]+");
+         System.out.println(tokens.length);
+         
+         for (String token : tokens) {
+            System.out.println(token);
+         }
+        }
+        
+        scan.close();
+    }
 }
