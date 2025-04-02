7c88f31ab1a2a3d756c27a0f42e1fd6449ddf50f
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-02 22:41:30 +0530

    Update Java_Regex.java

---
+++ Java_Regex.java
@@ -0,0 +1,19 @@
+class Java_Regex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex {
+    String pattern = "^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|0?[0-9]{1,2})\\."
+                   + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|0?[0-9]{1,2})\\."
+                   + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|0?[0-9]{1,2})\\."
+                   + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|0?[0-9]{1,2})$";
+}
