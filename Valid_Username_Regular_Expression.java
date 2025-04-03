ec198e10c65f5559bebe3f17c6146a2d76e9cfcb
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-03 13:37:09 +0530

    Update Valid_Username_Regular_Expression.java

--- Valid_Username_Regular_Expression.java
+++ Valid_Username_Regular_Expression.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class UsernameValidator {
     /*
      * Write regular expression here.
@@ -6,5 +8,18 @@ class UsernameValidator {
 }
 
 public class Valid_Username_Regular_Expression {
+    private static final Scanner scan = new Scanner(System.in);
     
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
 }
