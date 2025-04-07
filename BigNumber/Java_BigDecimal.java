a08a819ea3725cbcacae1bbac8d93045d6c6b75c
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-07 22:29:36 +0530

    Update Java_BigDecimal.java

--- BigNumber/Java_BigDecimal.java
+++ BigNumber/Java_BigDecimal.java
@@ -1,5 +1,27 @@
 package BigNumber;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Java_BigDecimal {
-    
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+        //Write your code here
+         Arrays.sort(s, (a, b) -> new BigDecimal(b).compareTo(new BigDecimal(a)));
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
 }
