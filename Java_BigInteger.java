8c1cd4cb080950cb848647acb7c33b72a351158f
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-15 23:47:13 +0530

    Update Java_BigInteger.java

--- Java_BigInteger.java
+++ Java_BigInteger.java
@@ -1,3 +1,23 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Java_BigInteger {
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the two large numbers as strings
+        BigInteger A = new BigInteger(scanner.next());
+        BigInteger B = new BigInteger(scanner.next());
+
+        // Perform addition and multiplication
+        BigInteger sum = A.add(B);
+        BigInteger product = A.multiply(B);
+
+        // Print results
+        System.out.println(sum);
+        System.out.println(product);
+
+        scanner.close();
+    }
 }
