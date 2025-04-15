7dfcf46abbd97ef69459843104ae387f948860ce
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-15 23:40:32 +0530

    Update Java_Primality_Test.java

--- Java_Primality_Test.java
+++ Java_Primality_Test.java
@@ -1,3 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
 public class Java_Primality_Test {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        
+        BigInteger bigInt = new BigInteger(n);
+        if (bigInt.isProbablePrime(10)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
 }
