729b6d40805207c2e25246f388f84c5cf8d1ee46
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-17 22:05:12 +0530

    Update Java_Exception_Handling.java

--- Java_Exception_Handling.java
+++ Java_Exception_Handling.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class MyCalculator {
     /*
     * Create the method long power(int, int) here.
@@ -15,5 +17,19 @@ class MyCalculator {
 }
 
 public class Java_Exception_Handling {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
     
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
