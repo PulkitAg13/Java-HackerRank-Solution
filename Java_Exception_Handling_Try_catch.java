2f2d0cb1120634476b4d3ddd9fd7c6798f5f35c3
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-17 21:59:26 +0530

    Update Java_Exception_Handling_Try_catch.java

--- Java_Exception_Handling_Try_catch.java
+++ Java_Exception_Handling_Try_catch.java
@@ -1,3 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Java_Exception_Handling_Try_catch {
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        try{
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int result = x / y;
+            System.out.println(result);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
 }
