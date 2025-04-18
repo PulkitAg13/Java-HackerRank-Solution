cd28ce3212155f9f45b200da1b234963432dd863
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-18 22:39:55 +0530

    Update Java_Inheritance_I.java

--- Java_Inheritance_I.java
+++ Java_Inheritance_I.java
@@ -1,3 +1,13 @@
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    
+    void sing(){
+		System.out.println("I am singing");
+	}
+}
+
 public class Java_Inheritance_I {
     
 }
