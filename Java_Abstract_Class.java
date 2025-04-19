f3785e599e6aa3bc8d56775a64ad008316a29224
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-20 00:06:43 +0530

    Update Java_Abstract_Class.java

--- Java_Abstract_Class.java
+++ Java_Abstract_Class.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 abstract class Book{
 	String title;
 	abstract void setTitle(String s);
@@ -14,5 +16,14 @@ class MyBook extends Book{
 }
 
 public class Java_Abstract_Class {
-    
+    public static void main(String []args){
+		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+		
+	}
 }
