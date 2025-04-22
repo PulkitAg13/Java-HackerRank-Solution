fa166d9a69d4b0263590312c8407574d18cc1e24
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-22 23:30:19 +0530

    Update Java_Interface.java

--- Java_Interface.java
+++ Java_Interface.java
@@ -2,7 +2,17 @@ interface AdvancedArithmetic{
     int divisor_sum(int n);
   }
 
-  
+class MyCalculator implements AdvancedArithmetic{
+    int sum = 0;
+    public int divisor_sum(int n){
+        for (int i = 1; i <= n; i++){
+            if(n % i == 0){
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+}
 
 public class Java_Interface {
     
