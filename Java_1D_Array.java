9b6fc222cf724f6723ef6b635b525909787f8ccd
PulkitAg13 <pulkit.ag13@gmail.com>
2025-04-30 19:46:37 +0530

    Update Java_1D_Array.java

--- Java_1D_Array.java
+++ Java_1D_Array.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Java_1D_Array {
-    
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++){
+            int x = scan.nextInt();
+            a[i] = x;
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
 }
