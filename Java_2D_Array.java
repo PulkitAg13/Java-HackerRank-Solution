400aa61c056c46012a10397189be55c09507ff08
PulkitAg13 <pulkit.ag13@gmail.com>
2025-05-01 19:15:36 +0530

    Update Java_2D_Array.java

--- Java_2D_Array.java
+++ Java_2D_Array.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Java_2D_Array {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
